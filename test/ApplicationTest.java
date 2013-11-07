ab056a577b4bae951e284b8b8fbe9320f726ec89
a36310925 <xuwenlong1988@126.com>
2013-11-07 21:47:37 +0800

    I just want to try!

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -23,6 +23,7 @@ import static org.fest.assertions.Assertions.*;
 *
 * Simple (JUnit) tests that can call all parts of a play app.
 * If you are interested in mocking a whole application, see the wiki for more details.
+*I just want to try
 *
 */
 public class ApplicationTest {
