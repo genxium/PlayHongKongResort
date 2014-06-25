3c0dd1e889d0a2aae28287f315cd613cf24bd500
genxium <genxium@gmail.com>
2014-06-25 10:02:47 +0000

    Dumpped data.

---
+++ app/Database/hongkongresort-2014-06-26.sql
@@ -0,0 +1,409 @@
+-- MySQL dump 10.13  Distrib 5.6.17, for Linux (i686)
+--
+-- Host: localhost    Database: hongkongweb
+-- ------------------------------------------------------
+-- Server version	5.6.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Activity`
+--
+
+DROP TABLE IF EXISTS `Activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Activity` (
+  `ActivityId` int(32) NOT NULL AUTO_INCREMENT,
+  `ActivityTitle` varchar(32) NOT NULL,
+  `ActivityContent` mediumtext,
+  `ActivityCreatedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ActivityBeginTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ActivityApplicationDeadline` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ActivityCapacity` int(32) NOT NULL DEFAULT '0',
+  `ActivityStatus` int(3) NOT NULL DEFAULT '0',
+  `HostId` int(32) NOT NULL,
+  PRIMARY KEY (`ActivityId`),
+  KEY `Activity_ibfk_1` (`HostId`),
+  CONSTRAINT `Activity_ibfk_1` FOREIGN KEY (`HostId`) REFERENCES `User` (`UserId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Activity`
+--
+
+LOCK TABLES `Activity` WRITE;
+/*!40000 ALTER TABLE `Activity` DISABLE KEYS */;
+INSERT INTO `Activity` VALUES (1,'Test','lalala','2014-06-25 09:30:45','2014-01-01 00:00:00','2014-01-01 00:00:00',0,0,1);
+/*!40000 ALTER TABLE `Activity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ActivityImageRelationTable`
+--
+
+DROP TABLE IF EXISTS `ActivityImageRelationTable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ActivityImageRelationTable` (
+  `ActivityImageRelationTableId` int(32) NOT NULL AUTO_INCREMENT,
+  `ActivityId` int(32) NOT NULL,
+  `ImageId` int(32) NOT NULL,
+  `GeneratedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ActivityImageRelationTableId`),
+  KEY `FK_ActivityId` (`ActivityId`),
+  KEY `FK_ImageId` (`ImageId`),
+  CONSTRAINT `ActivityImageRelationTable_ibfk_1` FOREIGN KEY (`ActivityId`) REFERENCES `Activity` (`ActivityId`),
+  CONSTRAINT `ActivityImageRelationTable_ibfk_2` FOREIGN KEY (`ImageId`) REFERENCES `Image` (`ImageId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ActivityImageRelationTable`
+--
+
+LOCK TABLES `ActivityImageRelationTable` WRITE;
+/*!40000 ALTER TABLE `ActivityImageRelationTable` DISABLE KEYS */;
+INSERT INTO `ActivityImageRelationTable` VALUES (1,1,2,'2014-06-25 09:30:45');
+/*!40000 ALTER TABLE `ActivityImageRelationTable` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CommentOnActivity`
+--
+
+DROP TABLE IF EXISTS `CommentOnActivity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CommentOnActivity` (
+  `CommentAId` int(32) NOT NULL AUTO_INCREMENT,
+  `CommentAContent` varchar(1024) NOT NULL,
+  `CommenterId` int(32) NOT NULL,
+  `ActivityId` int(32) NOT NULL,
+  `PredecessorId` int(32) NOT NULL DEFAULT '-1',
+  `CommentType` int(3) NOT NULL DEFAULT '0',
+  `GeneratedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ParentID` int(32) NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`CommentAId`),
+  KEY `FK_CommenterId` (`CommenterId`),
+  KEY `FK_ActivityId` (`ActivityId`),
+  CONSTRAINT `CommentOnActivity_ibfk_1` FOREIGN KEY (`CommenterId`) REFERENCES `User` (`UserId`),
+  CONSTRAINT `CommentOnActivity_ibfk_2` FOREIGN KEY (`ActivityId`) REFERENCES `Activity` (`ActivityId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CommentOnActivity`
+--
+
+LOCK TABLES `CommentOnActivity` WRITE;
+/*!40000 ALTER TABLE `CommentOnActivity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CommentOnActivity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Image`
+--
+
+DROP TABLE IF EXISTS `Image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Image` (
+  `ImageId` int(32) NOT NULL AUTO_INCREMENT,
+  `ImageURL` varchar(64) NOT NULL,
+  PRIMARY KEY (`ImageId`),
+  UNIQUE KEY `ImageURL` (`ImageURL`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Image`
+--
+
+LOCK TABLES `Image` WRITE;
+/*!40000 ALTER TABLE `Image` DISABLE KEYS */;
+INSERT INTO `Image` VALUES (1,'/images/UID1_1403541670715_8cb643c8526bc5fffc7348779412545e.png'),(2,'/images/UID1_1403688645553_e76a27b12f5d3a56663f6b6ac331b1a5.jpeg');
+/*!40000 ALTER TABLE `Image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Notification`
+--
+
+DROP TABLE IF EXISTS `Notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Notification` (
+  `Id` int(32) NOT NULL AUTO_INCREMENT,
+  `IsRead` int(2) DEFAULT '0',
+  `UserId` int(32) DEFAULT NULL,
+  `Content` varchar(128) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_Notification_ibfk_1` (`UserId`),
+  CONSTRAINT `FK_Notification_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `User` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Notification`
+--
+
+LOCK TABLES `Notification` WRITE;
+/*!40000 ALTER TABLE `Notification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Notification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `UserId` int(32) NOT NULL AUTO_INCREMENT,
+  `DisplayName` varchar(32) DEFAULT NULL,
+  `UserPassword` varchar(32) NOT NULL,
+  `UserEmail` varchar(32) NOT NULL,
+  `UserGroupId` int(2) NOT NULL DEFAULT '0',
+  `UserAuthenticationStatus` int(2) NOT NULL DEFAULT '0',
+  `UserGender` int(1) NOT NULL DEFAULT '0',
+  `UserLastLoggedInTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserAvatar` int(32) NOT NULL DEFAULT '0',
+  `UserLastLoggedOutTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserLastExitTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserCreatedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Username` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `UserEmail` (`UserEmail`),
+  UNIQUE KEY `UserName` (`Username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,NULL,'e10adc3949ba59abbe56e057f20f883e','genxium@gmail.com',1,0,0,'2014-04-28 17:37:38',1,'2014-04-28 17:37:38','2014-04-28 17:37:38','2014-04-28 17:37:38','genxium'),(4,NULL,'e10adc3949ba59abbe56e057f20f883e','genxium@hotmail.com',1,0,0,'2014-04-28 17:59:35',22,'2014-04-28 17:59:35','2014-04-28 17:59:35','2014-04-28 17:59:35','genxium1988'),(5,NULL,'c33367701511b4f6020ec61ded352059','admin@hongkongresort.com',3,0,0,'2014-04-28 18:14:22',0,'2014-04-28 18:14:22','2014-04-28 18:14:22','2014-04-28 18:14:22','admin'),(6,NULL,'e10adc3949ba59abbe56e057f20f883e','ray@gmail.com',1,0,0,'2014-04-29 16:20:56',23,'2014-04-29 16:20:56','2014-04-29 16:20:56','2014-04-29 16:20:56','ray');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `UserActivityRelationTable`
+--
+
+DROP TABLE IF EXISTS `UserActivityRelationTable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserActivityRelationTable` (
+  `UserActivityRelationTableId` int(32) NOT NULL AUTO_INCREMENT,
+  `UserId` int(32) NOT NULL,
+  `ActivityId` int(32) NOT NULL,
+  `Relation` int(3) NOT NULL,
+  `GeneratedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `LastApplyingTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `LastAcceptedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `LastRejectedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UserActivityRelationTableId`),
+  UNIQUE KEY `UA_UNI_ID` (`UserId`,`ActivityId`),
+  KEY `UserId` (`UserId`),
+  KEY `ActivityId` (`ActivityId`),
+  CONSTRAINT `UserActivityRelationTable_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `User` (`UserId`),
+  CONSTRAINT `UserActivityRelationTable_ibfk_2` FOREIGN KEY (`ActivityId`) REFERENCES `Activity` (`ActivityId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `UserActivityRelationTable`
+--
+
+LOCK TABLES `UserActivityRelationTable` WRITE;
+/*!40000 ALTER TABLE `UserActivityRelationTable` DISABLE KEYS */;
+INSERT INTO `UserActivityRelationTable` VALUES (1,1,1,1,'2014-06-25 09:30:45','2014-06-25 09:30:45','2014-06-25 09:30:45','2014-06-25 09:30:45');
+/*!40000 ALTER TABLE `UserActivityRelationTable` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `innodb_index_stats`
+--
+
+DROP TABLE IF EXISTS `innodb_index_stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `innodb_index_stats` (
+  `database_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `index_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `stat_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `stat_value` bigint(20) unsigned NOT NULL,
+  `sample_size` bigint(20) unsigned DEFAULT NULL,
+  `stat_description` varchar(1024) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`database_name`,`table_name`,`index_name`,`stat_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin STATS_PERSISTENT=0;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `innodb_index_stats`
+--
+
+LOCK TABLES `innodb_index_stats` WRITE;
+/*!40000 ALTER TABLE `innodb_index_stats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `innodb_index_stats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `innodb_table_stats`
+--
+
+DROP TABLE IF EXISTS `innodb_table_stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `innodb_table_stats` (
+  `database_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `n_rows` bigint(20) unsigned NOT NULL,
+  `clustered_index_size` bigint(20) unsigned NOT NULL,
+  `sum_of_other_index_sizes` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`database_name`,`table_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin STATS_PERSISTENT=0;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `innodb_table_stats`
+--
+
+LOCK TABLES `innodb_table_stats` WRITE;
+/*!40000 ALTER TABLE `innodb_table_stats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `innodb_table_stats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slave_master_info`
+--
+
+DROP TABLE IF EXISTS `slave_master_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slave_master_info` (
+  `Number_of_lines` int(10) unsigned NOT NULL COMMENT 'Number of lines in the file.',
+  `Master_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'The name of the master binary log currently being read from the master.',
+  `Master_log_pos` bigint(20) unsigned NOT NULL COMMENT 'The master log position of the last read event.',
+  `Host` char(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT 'The host name of the master.',
+  `User_name` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The user name used to connect to the master.',
+  `User_password` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The password used to connect to the master.',
+  `Port` int(10) unsigned NOT NULL COMMENT 'The network port used to connect to the master.',
+  `Connect_retry` int(10) unsigned NOT NULL COMMENT 'The period (in seconds) that the slave will wait before trying to reconnect to the master.',
+  `Enabled_ssl` tinyint(1) NOT NULL COMMENT 'Indicates whether the server supports SSL connections.',
+  `Ssl_ca` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The file used for the Certificate Authority (CA) certificate.',
+  `Ssl_capath` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The path to the Certificate Authority (CA) certificates.',
+  `Ssl_cert` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The name of the SSL certificate file.',
+  `Ssl_cipher` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The name of the cipher in use for the SSL connection.',
+  `Ssl_key` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The name of the SSL key file.',
+  `Ssl_verify_server_cert` tinyint(1) NOT NULL COMMENT 'Whether to verify the server certificate.',
+  `Heartbeat` float NOT NULL,
+  `Bind` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'Displays which interface is employed when connecting to the MySQL server',
+  `Ignored_server_ids` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The number of server IDs to be ignored, followed by the actual server IDs',
+  `Uuid` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The master server uuid.',
+  `Retry_count` bigint(20) unsigned NOT NULL COMMENT 'Number of reconnect attempts, to the master, before giving up.',
+  `Ssl_crl` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The file used for the Certificate Revocation List (CRL)',
+  `Ssl_crlpath` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT 'The path used for Certificate Revocation List (CRL) files',
+  `Enabled_auto_position` tinyint(1) NOT NULL COMMENT 'Indicates whether GTIDs will be used to retrieve events from the master.',
+  PRIMARY KEY (`Host`,`Port`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 STATS_PERSISTENT=0 COMMENT='Master Information';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slave_master_info`
+--
+
+LOCK TABLES `slave_master_info` WRITE;
+/*!40000 ALTER TABLE `slave_master_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slave_master_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slave_relay_log_info`
+--
+
+DROP TABLE IF EXISTS `slave_relay_log_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slave_relay_log_info` (
+  `Number_of_lines` int(10) unsigned NOT NULL COMMENT 'Number of lines in the file or rows in the table. Used to version table definitions.',
+  `Relay_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'The name of the current relay log file.',
+  `Relay_log_pos` bigint(20) unsigned NOT NULL COMMENT 'The relay log position of the last executed event.',
+  `Master_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'The name of the master binary log file from which the events in the relay log file were read.',
+  `Master_log_pos` bigint(20) unsigned NOT NULL COMMENT 'The master log position of the last executed event.',
+  `Sql_delay` int(11) NOT NULL COMMENT 'The number of seconds that the slave must lag behind the master.',
+  `Number_of_workers` int(10) unsigned NOT NULL,
+  `Id` int(10) unsigned NOT NULL COMMENT 'Internal Id that uniquely identifies this record.',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 STATS_PERSISTENT=0 COMMENT='Relay Log Information';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slave_relay_log_info`
+--
+
+LOCK TABLES `slave_relay_log_info` WRITE;
+/*!40000 ALTER TABLE `slave_relay_log_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slave_relay_log_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slave_worker_info`
+--
+
+DROP TABLE IF EXISTS `slave_worker_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slave_worker_info` (
+  `Id` int(10) unsigned NOT NULL,
+  `Relay_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `Relay_log_pos` bigint(20) unsigned NOT NULL,
+  `Master_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `Master_log_pos` bigint(20) unsigned NOT NULL,
+  `Checkpoint_relay_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `Checkpoint_relay_log_pos` bigint(20) unsigned NOT NULL,
+  `Checkpoint_master_log_name` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `Checkpoint_master_log_pos` bigint(20) unsigned NOT NULL,
+  `Checkpoint_seqno` int(10) unsigned NOT NULL,
+  `Checkpoint_group_size` int(10) unsigned NOT NULL,
+  `Checkpoint_group_bitmap` blob NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 STATS_PERSISTENT=0 COMMENT='Worker Information';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slave_worker_info`
+--
+
+LOCK TABLES `slave_worker_info` WRITE;
+/*!40000 ALTER TABLE `slave_worker_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slave_worker_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-06-25 10:02:32
