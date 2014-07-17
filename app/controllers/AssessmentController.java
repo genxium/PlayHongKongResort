e680e69c0ac72ecd14d8c7a4231ee05dfe6ad9d3
yflu <yflu@memoriki.com>
2014-07-17 15:02:25 +0800

    Added AssessmentController.

---
+++ app/controllers/AssessmentController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dao.SQLHelper;
+import model.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import play.libs.Json;
+import play.mvc.Content;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import utilities.DataUtils;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+public class AssessmentController extends Controller {
+	    
+}
