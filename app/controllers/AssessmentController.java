809457c23660b7a7d4230c190d987e5a53e350a2
genxium <genxium@hotmail.com>
2014-07-20 17:33:21 +0800

    Updated AssessmentController.

--- app/controllers/AssessmentController.java
+++ app/controllers/AssessmentController.java
@@ -2,30 +2,30 @@ package controllers;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import dao.SQLHelper;
-import dao.EasyPreparedStatementBuilder;
-import model.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
+import model.Assessment;
 import play.libs.Json;
-import play.mvc.Content;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
-import utilities.DataUtils;
 
-import java.sql.Timestamp;
-import java.util.*;
+import java.util.List;
 
 public class AssessmentController extends Controller {
     public static Result query(String refIndex, Integer numItems, Integer direction, String token, Integer userId, Integer activityId){
         response().setContentType("text/plain");
-        do{
-            try{
-                return ok();
-            } catch(Exception e){
-                System.out.println(AssessmentController.class.getName()+".query, "+e.getCause());
+        try{
+            List<Assessment> assessments=SQLCommander.queryAssessments(refIndex, Assessment.GENERATED_TIME, SQLHelper.DESCEND, numItems, direction, activityId);
+            ObjectNode result = Json.newObject();
+            for(Assessment assessment : assessments){
+                result.put(String.valueOf(assessment.getId()), assessment.toObjectNode());
             }
-        } while(false);
+            return ok(result);
+        } catch(Exception e){
+            System.out.println(AssessmentController.class.getName()+".query, "+e.getCause());
+        }
         return badRequest();
+    }
+
+    public static Result submit(Integer activityId, String token, String bundle){
+        return ok();
     }
 }
