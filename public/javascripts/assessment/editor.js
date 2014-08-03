240ac90026e7c7ca1b5329dac405cdb6870565f9
genxium <genxium@hotmail.com>
2014-08-04 01:15:02 +0800

    Updated assessment editor.

--- public/javascripts/assessment/editor.js
+++ public/javascripts/assessment/editor.js
@@ -1,12 +1,43 @@
 var g_assessmentEditor=null;
 
+/*
+	generateAssessmentEditor(participants: User[])
+*/
 function generateAssessmentEditor(participants){
 	var ret=$("<div>");
 	for(var i=0;i<participants.length;i++){
 		var participant=participants[i];
-		var name=$("<plaintext>", {
-			text: participant.name
-		}).appendTo(ret);	
+		var row=generateSingleAssessmentEditor(participant);	
+		ret.append(row);
 	}	
 	return ret;
 }
+
+/*
+	generateSingleAssessmentEditor(participant: User)
+*/
+function generateSingleAssessmentEditor(participant){
+	var ret=$('<p>');
+	var name=$('<plaintext>', {
+		text: participant.name
+		class: 'name'
+	}).appendTo(ret);	
+	var content=$('<input>', {
+		type: 'text'
+		class: 'content'	
+	}).appendTo(ret); 
+	var lock=$('<checkbox>', {
+		style: 'color: red',	
+		class: 'lock'
+	}).appendTo(ret);
+	return ret;	
+}
+
+/*
+*/
+function getAssessment(singleEditor){
+	var name=$(singleEditor.children('.name')[0]).attr('text');	
+	var content=$(singleEditor.children('.content')[0]).val();
+	var lock=$(singleEditor.children('.lock')[0]).is(':checked');
+}
+
