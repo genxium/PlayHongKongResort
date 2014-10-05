a2d7fa7468ec29af6d3a3ecdbc667744f090d12d
wing <wing@hongkongresort.com>
2014-10-05 13:30:13 -0400

    Added script to backup individual database tables.

---
+++ database/backup_individual_tables.sh
@@ -0,0 +1,11 @@
+mysqldump -uroot -d hongkongresort activity>create_table_activity.sql
+mysqldump -uroot -d hongkongresort activity_image_relation>create_table_activity_image_relation.sql
+mysqldump -uroot -d hongkongresort assessment>create_table_assessment.sql
+mysqldump -uroot -d hongkongresort comment>create_table_comment.sql
+mysqldump -uroot -d hongkongresort image>create_table_image.sql
+mysqldump -uroot -d hongkongresort login>create_table_login.sql
+mysqldump -uroot -d hongkongresort notification>create_table_notification.sql 
+mysqldump -uroot -d hongkongresort user>create_table_user.sql
+mysqldump -uroot -d hongkongresort user_activity_relation>create_table_user_activity_relation.sql
+ 
+
