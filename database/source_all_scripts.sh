36d9165b3f27680101a18683ae919ae7c656c3ed
yflu <yflu@memoriki.com>
2014-11-10 15:20:11 +0800

    Added db script.

---
+++ database/source_all_scripts.sh
@@ -0,0 +1,13 @@
+mysql -uroot hongkongresort < create_table_user.sql;
+mysql -uroot hongkongresort < create_table_activity.sql;
+mysql -uroot hongkongresort < create_table_image.sql;
+mysql -uroot hongkongresort < create_table_comment.sql;
+mysql -uroot hongkongresort < create_table_assessment.sql;
+mysql -uroot hongkongresort < create_table_login.sql;
+mysql -uroot hongkongresort < create_table_notification.sql;
+mysql -uroot hongkongresort < create_table_user_activity_relation.sql;
+mysql -uroot hongkongresort < create_table_activity_image_relation.sql;
+
+
+
+
