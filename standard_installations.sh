a833d57ae41464a469c1802ee9455fe1249dbc0e
genxium <genxium@hotmail.com>
2015-01-25 13:23:19 -0500

    Added script standard_installations.sh.

---
+++ standard_installations.sh
@@ -0,0 +1,27 @@
+# install nginx by yum repo, reference: http://wiki.nginx.org/Install#Official_Red_Hat.2FCentOS_packages
+echo -e '[nginx]\nname=nginx repo\nbaseurl=http://nginx.org/packages/centos/$releasever/$basearch/\ngpgcheck=0\nenabled=1' >> /etc/yum.repos.d/nginx.repo
+yum install nginx
+
+# install mysql-server by yum repo, reference: http://dev.mysql.com/downloads/repo/yum/
+yum install wget
+wget http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
+rpm -Uvh mysql-community-release-el6-5.noarch.rpm
+yum install mysql-server
+
+# install system logging utility `sar`
+yum install sar
+
+# install jdk
+java-1.7.0-openjdk-devel
+
+# install vim editor
+yum install vim
+
+# install git
+yum install git
+
+# install mail utility `postfix`
+yum install postfix
+
+# install other tools
+yum install unzip libtool autoconf automake
