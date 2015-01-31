cd0985f03029a145ccc4a3bc0c89ecb875f54dc0
wing <wing@hongkongresort.com>
2015-01-30 23:23:09 -0500

    Minor fix.

--- public/javascripts/participant/editor.js
+++ public/javascripts/participant/editor.js
@@ -16,7 +16,8 @@ function generateParticipantsSelectionForm(par, activity) {
 		class: "participant-form"
 	}).appendTo(par);
 
-	var editable = (g_loggedInUser != null && g_loggedInUser.id == activity.host.id && currentMillis() > activity.beginTime);
+	var currentGmTMillis = currentMillis();
+	var editable = (g_loggedInUser != null && g_loggedInUser.id == activity.host.id && currentGmTMillis < activity.beginTime);
 
 	var labels = new Array();
 	var boxes = new Array();
