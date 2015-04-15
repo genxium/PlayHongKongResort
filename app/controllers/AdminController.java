cc54fcc45a1f626ba1b6924a945b2acc6e93ee21
genxium <genxium@gmail.com>
2015-04-15 23:04:36 +0800

    Updated AdminController.

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -1,15 +1,17 @@
 package controllers;
 
 import components.TokenExpiredResult;
-import exception.AccessDeniedException;
-import exception.ActivityNotFoundException;
-import exception.TokenExpiredException;
-import exception.UserNotFoundException;
+import exception.*;
+import models.AbstractMessage;
 import models.Activity;
 import models.User;
 import models.UserActivityRelation;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utilities.Converter;
 import utilities.Loggy;
 
 import java.util.Map;
@@ -21,7 +23,6 @@ public class AdminController extends Controller {
     public static Result accept() {
 	    try {
 		    Map<String, String[]> formData = request().body().asFormUrlEncoded();
-		    Integer activityId = Integer.valueOf(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
 		    String token = formData.get(User.TOKEN)[0];
 
 		    Long userId = DBCommander.queryUserId(token);
@@ -30,7 +31,9 @@ public class AdminController extends Controller {
 		    if (user == null) throw new UserNotFoundException();
 		    if (!DBCommander.validateAdminAccess(user)) throw new AccessDeniedException();
 
-		    Activity activity = DBCommander.queryActivity(activityId);
+			Long activityId = Converter.toLong(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
+			if (activityId == null) throw new InvalidQueryParamsException();
+			Activity activity = DBCommander.queryActivity(activityId);
 		    if (activity == null) throw new ActivityNotFoundException();
 
 		    if(!DBCommander.acceptActivity(user, activity)) throw new NullPointerException();
@@ -47,7 +50,6 @@ public class AdminController extends Controller {
     public static Result reject() {
 	    try {
             Map<String, String[]> formData = request().body().asFormUrlEncoded();
-            Integer activityId = Integer.valueOf(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
             String token = formData.get(User.TOKEN)[0];
 
             Long userId = DBCommander.queryUserId(token);
@@ -56,7 +58,9 @@ public class AdminController extends Controller {
             if (user == null) throw new UserNotFoundException();
             if (!DBCommander.validateAdminAccess(user)) throw new AccessDeniedException();
 
-            Activity activity = DBCommander.queryActivity(activityId);
+			Long activityId = Converter.toLong(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
+			if (activityId == null) throw new InvalidQueryParamsException();
+			Activity activity = DBCommander.queryActivity(activityId);
             if (activity == null) throw new ActivityNotFoundException();
 
             if(!DBCommander.rejectActivity(user, activity)) throw new NullPointerException();
@@ -72,7 +76,6 @@ public class AdminController extends Controller {
     public static Result delete() {
 	    try {
 		    Map<String, String[]> formData = request().body().asFormUrlEncoded();
-		    Integer activityId = Integer.valueOf(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
 		    String token = formData.get(User.TOKEN)[0];
 
 		    Long userId = DBCommander.queryUserId(token);
@@ -82,7 +85,9 @@ public class AdminController extends Controller {
 		    if (user == null) throw new UserNotFoundException();
 		    if (!DBCommander.validateAdminAccess(user)) throw new AccessDeniedException();
 
-		    if(!ExtraCommander.deleteActivity(activityId)) throw new NullPointerException();
+			Long activityId = Converter.toLong(formData.get(UserActivityRelation.ACTIVITY_ID)[0]);
+			if (activityId == null) throw new InvalidQueryParamsException();
+			if(!ExtraCommander.deleteActivity(activityId)) throw new NullPointerException();
 
 		    return ok();
 	    } catch (TokenExpiredException e) {
@@ -92,4 +97,32 @@ public class AdminController extends Controller {
 	    }
 	    return badRequest();
     }
+
+	public static Result prioritize() {
+		try {
+			Map<String, String[]> formData = request().body().asFormUrlEncoded();
+			String token = formData.get(User.TOKEN)[0];
+
+			Long userId = DBCommander.queryUserId(token);
+			if (userId == null) throw new UserNotFoundException();
+
+			User user = DBCommander.queryUser(userId);
+			if (user == null) throw new UserNotFoundException();
+			if (!DBCommander.validateAdminAccess(user)) throw new AccessDeniedException();
+
+			JSONArray bundle= (JSONArray) JSONValue.parse(formData.get(AbstractMessage.BUNDLE)[0]);
+			for (Object obj : bundle) {
+				Activity activity = new Activity((JSONObject) obj);
+				/**
+				 * TODO: update activity priority settings
+				 * */
+			}
+			return ok();
+		} catch (TokenExpiredException e) {
+			return ok(TokenExpiredResult.get());
+		} catch (Exception e) {
+			Loggy.e(TAG, "prioritize", e);
+		}
+		return badRequest();
+	}
 }
