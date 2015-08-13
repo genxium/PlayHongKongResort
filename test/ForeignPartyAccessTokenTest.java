61dd2022f9391d28c6e55c4bcd034478dbc39d2b
genxium <genxium@gmail.com>
2015-08-13 19:13:20 +0800

    Added mockito support.

--- test/ForeignPartyAccessTokenTest.java
+++ test/ForeignPartyAccessTokenTest.java
@@ -1,2 +1,18 @@
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import components.StandardSuccessResult;
+import controllers.ForeignPartyController;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class ForeignPartyAccessTokenTest {
+
+    @Test
+    public void testLogin() {
+        ForeignPartyController mockController = mock(ForeignPartyController.class);
+        ObjectNode stdSuccess = StandardSuccessResult.get();
+        doReturn(stdSuccess).when(mockController).login();
+        assertEquals(stdSuccess, mockController.login());
+    }
 }
