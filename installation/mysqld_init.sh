cc546525b7f78775f69d6152e36ab9bc61acbd29
Wing <genxium@hotmail.com>
2015-08-14 16:53:48 +0800

    Minor fix.

--- installation/mysqld_init.sh
+++ installation/mysqld_init.sh
@@ -1,5 +1,5 @@
 basedir=$(pwd)
 sudo su - root -c "mysql_install_db --user=mysql --ldata=/var/lib/mysql"
-sudo su - root -c "sh $basedir/mysqld_start.sh"
-sudo su - root -c "mysql_secure_installation"
 sudo su - root -c "cat $basedir/mysqld_template.conf > /etc/my.cnf"
+sudo su - root -c "sh $basedir/../mysqld_start.sh"
+sudo su - root -c "mysql_secure_installation"
