1e72a5dc14fe6a08b9e5c05c9f22c5f4c783e679
genxium <genxium@hotmail.com>
2015-12-11 00:51:01 +0800

    Bug fix for 'getCookie'.

--- public/javascripts/development/global_functions.js
+++ public/javascripts/development/global_functions.js
@@ -11,14 +11,18 @@ function setCookie(key, val, path, days) {
 }
 
 function getCookie(key) {
-	if (document.cookie.length <= 0) return null;
-	var start = document.cookie.indexOf(key + "=");
-	if (start == -1)	return null;
-	if (start - 1 >= 0 && (document.cookie[start - 1] != ' ' && document.cookie[start - 1] != ';')) return null;
-	start = start + key.length + 1;
-	end = document.cookie.indexOf(";", start);
-	if (end == -1)	end = document.cookie.length;
-	return unescape(document.cookie.substring(start, end));
+        if (document.cookie.length <= 0) return null;
+        var start = document.cookie.indexOf(key + "=");
+        var isValid = (start == 0 || document.cookie[start - 1] == ' ' || document.cookie[start - 1] == ';');
+        while (!isValid) {
+                if (start == -1)        return null;
+                start = document.cookie.indexOf(key + "=", start + 1); 
+                isValid = (start == 0 || document.cookie[start - 1] == ' ' || document.cookie[start - 1] == ';');
+        }
+        start = start + key.length + 1;
+        end = document.cookie.indexOf(";", start);
+        if (end == -1)  end = document.cookie.length;
+        return unescape(document.cookie.substring(start, end));
 }
 
 function removeCookie(key) {
